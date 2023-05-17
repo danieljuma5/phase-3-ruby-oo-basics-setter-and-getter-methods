f2101341b494c21e7902b1036c9535de9079f075
danieljuma5 <jumadaniel275@gmail.com>
2023-05-17 19:51:15 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
-class Dog
+require "pry"
 
+class Dog
+  #setter methods
+  def name=(name)
+    @name = name
+  end
+  def breed= (breed)
+    @breed = breed
+  end
+  #getter methods
+  def name 
+    @name
+  end
+  def breed
+    @breed
+  end
 end
+dog = Dog.new
